9d254f6cbc69615d523a688cc0dd89414cf3d8e7
Alex Fork <4ounder@gmail.com>
2013-09-30 21:00:16 +0300

    fixing `examples`
    - get rid of `examples` folder for a while
    
    Signed-off-by: Alex Fork <4ounder@gmail.com>

--- examples/test_http.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-http
-  .createServer(function(req, res) {
-    res.writeHeader(200, { 'Content-Type': 'text/plain; charset=utf8' });
-    res.end('response');
-  })
-  .listen(8080);
