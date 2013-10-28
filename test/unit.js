be6d43cdb3d8a3c85b895c7a1d94a4f5eb03cb23
Alex Fork <4ounder@gmail.com>
2013-10-28 18:36:09 +0200

    Version 1.0.4
    - BUG! - replacing multiple # with &
    - little refactoring
    - removed stupid examples (m8b let them back in 1.0.5)
    
    Signed-off-by: Alex Fork <4ounder@gmail.com>

--- test/unit.js
+++ test/unit.js
@@ -1,60 +1,11 @@
 var should  = require('should');
 var cleaner = require('..');
 
-var connect = function connect(opts) {
-  var options = opts || {};
-  var c = connect;
-
-  c._fn = null;
-
-  c._test = {
-    '_body': null,
-    '_code': null,
-    '_header': null,
-    '_next': null,
-    '_redirect': null,
-    '_url': null
-  };
-
-  c.req = {
-    'method': opts.method || 'GET',
-    'url': opts.url || '/'
-  };
-
-  c.res = {
-    'writeHead': function(code, headers) {
-      c._test._code = code;
-      c._test._url = headers['Location'];
-    },
-    'end': function(body) {
-      body && (c._test._body = body);
-      c._test._redirect = true;
-    }
-  };
-
-  c.next = function() {
-    c._test._next = true;
-  };
-
-  c.use = function(fn) {
-    c._fn = fn;
-    return c;
-  };
-
-  c.handle = function(cb) {
-    if (c._fn === null) {
-      throw Error('connect mock needs some middleware');
-    } else {
-      c._fn(c.req, c.res, c.next);
-      cb && cb(c._test);
-    }
-  };
-
-  return connect;
-};
+var utils = require('./utils');
+var connect = utils.connect;
 
 describe('cleaner', function() {
-  describe('options', function(done) {
+  describe('options', function() {
     var instance;
 
     before(function() {
---
+++ test/utils.js
@@ -0,0 +1,51 @@
+exports.connect = function connect(opts) {
+  // var options = opts || {};
+  var c = connect;
+
+  c._fn = null;
+
+  c._test = {
+    '_body': null,
+    '_code': null,
+    '_header': null,
+    '_next': null,
+    '_redirect': null,
+    '_url': null
+  };
+
+  c.req = {
+    'method': opts.method || 'GET',
+    'url': opts.url || '/'
+  };
+
+  c.res = {
+    'writeHead': function(code, headers) {
+      c._test._code = code;
+      c._test._url = headers.Location;
+    },
+    'end': function(body) {
+      if (body) c._test._body = body;
+      c._test._redirect = true;
+    }
+  };
+
+  c.next = function() {
+    c._test._next = true;
+  };
+
+  c.use = function(fn) {
+    c._fn = fn;
+    return c;
+  };
+
+  c.handle = function(cb) {
+    if (c._fn === null) {
+      throw Error('connect mock needs some middleware');
+    } else {
+      c._fn(c.req, c.res, c.next);
+      if (cb) cb(c._test);
+    }
+  };
+
+  return connect;
+};
